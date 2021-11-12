af618e3b41691e644f20140d325560fa4a49625c
xeth <adamhung12@gmail.com>
2021-11-12 14:10:43 +0800

    add options  -h,--help  -p,--print

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,22 @@
 use uuid::Uuid;
 use clipboard::{ClipboardContext, ClipboardProvider};
+use std::env;
+use std::process::exit;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let mut ctx: ClipboardContext = ClipboardProvider::new().unwrap();
+    let args:Vec<String> = env::args().collect();
     let my_uuid = Uuid::new_v4();
+    if args.len() == 2 {
+        if "-p" == &args[1] || "--print" == &args[1] {
+            println!("{}", my_uuid);
+        }
+        else if "-h" == &args[1] || "--help" == &args[1] {
+            println!("xhuuid [-p|--print] [-h|--help]");
+            exit(0)
+        }
+    }
+
+    let mut ctx: ClipboardContext = ClipboardProvider::new().unwrap();
     let rs = ctx.set_contents(my_uuid.to_string());
     return rs
-    // println!("{}", my_uuid);
-    // Ok(())
 }
